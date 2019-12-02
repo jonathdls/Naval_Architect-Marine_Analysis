f16d7d05eef0b2740e8306ae5ff62d1890510723
Einar <ebg@sevassp.com>
2019-12-02 22:23:18 +0100

    Added task 2

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ Hints and necessary code is found in the repository. All code is written in Pyth
 Download the quiz sheet from this repository and add your answers to the questions below.  
 
 ### 1) Incorrect Froude scaling
+Model testing is a vital part of verifying the performance of a floating structure. 
+Reults in model scale are converted to full scale under the assumption that the Froude number in both model and full scale is the same.
+
 The complied python file for task 1 contains the following function:
 
 ```python
@@ -31,7 +34,16 @@ def model_test_scaling(full_scale_diameter):
 The model test scaling produces incorrect results for one of the channels.
 Which channel scales incorrectly, and why is it incorrect? 
 
-### 2)
+### 2) Mooring chain properties
+Chains are important components in the mooring system use to keep floating structures in place.
+
+The task 2 Python file contains a simplified ```MooringChain``` class capable of returning the breaking strength and dry weight of chain in accordance with DNVGL-OS-E302 (or any other class society for tha matter).
+This class can be used to answer he following questions:
+
+What is the breaking strength of 157 mm R4S chain in kN?
+What is the dry weight per length of 84 mm stud chain in kg/m?
+
+Be aware that the SI units are expected and returned by the ```MooringChain``` class and its methods.   
 
 ### 3)
 
---
+++ tasks/task2.py
@@ -0,0 +1,65 @@
+class MooringChain:
+    """
+    Model of mooring chain cross section
+    Parameters
+    ----------
+    quality : str
+        Chain material quality (R3, R3S, R4, R4S or R5)
+    stud : bool
+        Studded chain (True) or studless chain (False)
+    References
+    ----------
+    .. [1] DNVGL-OS-E302, "Offshore mooring chain", 2015
+    .. [2] www.vicinay-cadenas.net/mooring-chain/offshore-mooring-chain.asp
+    """
+
+    def __init__(self, quality: str, stud: bool):
+        self.stud = stud
+        if quality.upper() not in ['R3', 'R3S', 'R4', 'R4S', 'R5']:
+            raise ValueError(f"Invalid chain quality: '{quality.upper()}'")
+        self.quality = quality.upper()
+
+    def factor_strength(self):
+        """float: Ratio of breaking strength to `diameter^2 * (44 - 0.08*diameter)` per [1] for the
+        specified material quality (kN / mm^2 - ich)."""
+        quality_strength = dict(R3=0.0223, R3S=0.0249, R4=0.0274, R4S=0.0304, R5=0.0320)
+        return quality_strength[self.quality]
+
+    def factor_dry_weight(self):
+        """float: Ratio of unit length dry weight to diameter squared per [1] and [2] (N / m^2 / m)."""
+        if self.stud:
+            return 0.0219 * 1.e6 * 9.81
+        elif not self.stud:
+            return 0.0200 * 1.e6 * 9.81
+
+    def breaking_strength(self, diameter: float):
+        """
+        Chain breaking strength aka. MBL.
+        Parameters
+        ----------
+        diameter : float
+            Cross sectiom diameter (m).
+        Returns
+        -------
+        float
+            Cross section breaking strength (N).
+        Notes
+        -----
+        The strength is calculated as `factor_strength * (diameter*1000)^2 * (44 - 0.08*diameter*1000) * 1000`
+        (with SI units). See [1].
+        """
+        return self.factor_strength() * pow(diameter * 1000, 2) * (44 - 0.08 * diameter * 1000) * 1000
+
+    def dry_weight(self, diameter: float):
+        """
+        Cross section dry weight per unit length.
+        Parameters
+        ----------
+        diameter : float
+            Diameter (m) of cross section for which the corresponding break load should be returned
+        Returns
+        -------
+        float
+            Dry weight per unit length of chain (N / m)
+        """
+        return self.factor_dry_weight() * pow(diameter, 2)
