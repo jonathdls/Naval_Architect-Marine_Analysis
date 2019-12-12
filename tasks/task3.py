a367ae29bbc3a3de0cc5410a94f59f17f15d2078
larsmr <lmr@sevanssp.com>
2019-12-12 19:34:26 +0100

    new setup where one initiate SimplifiedBarge and SimplifiedCylinder with geometric properties

--- tasks/task3.py
+++ tasks/task3.py
@@ -16,6 +16,7 @@ class SimplifiedFloater:
 
     def __init__(self):
         self.est_added_mass = 0
+        self.wp_area = 0
 
     def natural_period_heave(self, mass, waterplane_area=None, added_mass=None):
         """
@@ -28,51 +29,86 @@ class SimplifiedFloater:
             estimated added mass in heave for structure [t]
         :return:
         """
-        if added_mass is not None:
+        if added_mass is not None and waterplane_area is not None:
             return round(2 * np.pi * np.sqrt((mass + added_mass) / (self.rho * self.grav * waterplane_area)), 1)
-        else:
+        elif added_mass is not None and self.wp_area != 0:
+            return round(2 * np.pi * np.sqrt((mass + self.est_added_mass) / (self.rho * self.grav * self.wp_area)), 1)
+        elif waterplane_area is not None:
             return round(2 * np.pi * np.sqrt((mass + self.est_added_mass) / (self.rho * self.grav * waterplane_area)), 1)
+        elif self.wp_area != 0:
+            return round(2 * np.pi * np.sqrt((mass + self.est_added_mass) / (self.rho * self.grav * self.wp_area)), 1)
+        else:
+            return ValueError("Make sure to have a positive waterplane area, can be calculated through SimplfiedBarge"
+                              "or SimplifiedCylinder or manual input")
 
 
 class SimplifiedCylinder(SimplifiedFloater):
+    """
+    Simplified calculation of cylinder added mass, and calculation of waterplane area
+    Parameters
+    ----------
+    diameter : float
+        diameter of cylinder [m]
+    """
 
-    def __init__(self):
+    def __init__(self, diameter: float):
         super().__init__()
         self.est_added_mass = 0
+        self.diameter = diameter
+        self.est_added_mass = self.estimated_added_mass
+        self.wp_area = self.waterplane_area
 
-    def estimated_added_mass(self, diameter: float):
+    @property
+    def estimated_added_mass(self):
         """
         Estimated heave added mass of a cylinder.
-        diameter : float
-            Diameter of cylinder [m]
         return : Added mass [t]
 
         Notes
         -----
         The added mass for a cylinder is approximated by Lamb classical solution, 1932: A_33 = 1/3*rho*D^3
         """
-        if diameter > 0:
-            self.est_added_mass = 1 / 3 * self.rho * diameter ** 3
+        if self.diameter > 0:
+            self.est_added_mass = 1 / 3 * self.rho * self.diameter ** 3
             return round(self.est_added_mass, 1)
         else:
             raise ValueError("Make sure to have positive input scalar")
 
+    @property
+    def waterplane_area(self):
+        """
+        Waterplane area of cylinder
+        :return: Waterplane area of cylinder [m^2]
+        """
+        return np.pi * self.diameter**2 / 4
+
 
 class SimplifiedBarge(SimplifiedFloater):
+    """
+    Simplified calculation of barge added mass, and calculation of waterplane area
+    Parameters
+    ----------
+    width : float
+        width of barge [m]
+    draft : float
+        draft of barge [m]
+    length : float
+        length of barge [m]
+    """
 
-    def __init__(self):
+    def __init__(self, width: float, draft: float, length: float):
         super().__init__()
         self.est_added_mass = 0
+        self.width = width
+        self.draft = draft
+        self.length = length
+        self.est_added_mass = self.estimated_added_mass
+        self.wp_area = self.waterplane_area
 
-    def estimated_added_mass(self, width: float, draft: float, length: float):
+    @property
+    def estimated_added_mass(self):
         """
         Estimated heave added mass of a barge.
-        length : float
-            Length of barge [m]
-        width : float
-            Width of barge [m]
-        draft : float
-            Draft of barge [m]
         return : Added mass [t]
 
         Notes
@@ -80,18 +116,24 @@ class SimplifiedBarge(SimplifiedFloater):
         The added mass for a barge is approximated by Lewis numerical study
         A_33 = cm * pi * rho * (width/2)^2 * length
         """
-        if width > 0 and draft > 0 and length > 0:
-            self.est_added_mass = self.estimated_added_mass_coefficient(width, draft) \
-                                  * self.rho * np.pi * width**2 / 4 * length
-            return round(self.est_added_mass, 1)
+        if self.width > 0 and self.draft > 0 and self.length > 0:
+            return self.estimated_added_mass_coefficient \
+                                  * self.rho * np.pi * self.width**2 / 4 * self.length
         else:
             raise ValueError("Make sure to have positive input scalars")
 
-    def estimated_added_mass_coefficient(self, width: float, draft: float):
+    @property
+    def estimated_added_mass_coefficient(self):
         """
         Estimation of added mass coefficient (cm) for a rectangular 2d geometry
-        :param width: width of geometry
-        :param draft: draft of the geometry
         :return: estimated added mass coefficient of a rectangular approximated by line fit of Lewis numerical study
         """
-        return 1.5937 * (draft/width)**0.121
+        return 1.5937 * (self.draft/self.width)**0.121
+
+    @property
+    def waterplane_area(self):
+        """
+        Waterplane area of barge
+        :return: Waterplane area [m^2]
+        """
+        return self.width * self.length
